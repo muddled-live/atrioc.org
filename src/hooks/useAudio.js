a69b6d09ac04258ede8809618273d75eac761cbe
John Rogers <jrhawktwo@gmail.com>
2023-11-01 04:58:11 -0500

    initial commit

---
+++ src/hooks/useAudio.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+
+export const useAudio = (url) => {
+    const [audio] = useState(new Audio(url));
+    const [playing, setPlaying] = useState(false);
+
+    const toggle = () => {
+        setPlaying(!playing);
+        window.open("https://discord.gg/C5JaExrVCX", "_blank");
+    }
+
+    useEffect(() => {
+        playing ? audio.play() : audio.pause();
+    },
+        [playing]
+    );
+
+    useEffect(() => {
+        audio.addEventListener('ended', () => setPlaying(false));
+        return () => {
+            audio.removeEventListener('ended', () => setPlaying(false));
+        };
+    }, []);
+
+    return [toggle];
+};
