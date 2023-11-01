d4ad905a17f85586a0c5970dc0b61095e798315b
John Rogers <jrhawktwo@gmail.com>
2023-11-01 05:01:01 -0500

    added vercel analytics

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <React>
+        <Analytics />
         <App />
-    </React.StrictMode>
+    </React>
 );
