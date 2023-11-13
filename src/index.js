01769dacbc68c912309101fc73dbedd2187e3efa
Dylan Hackworth <dylhack@gmail.com>
2023-11-12 18:55:36 -0600

    Add CI & Formatter (#4)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Analytics } from '@vercel/analytics/react';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <Analytics />
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Analytics />
+    <App />
+  </React.StrictMode>,
 );
