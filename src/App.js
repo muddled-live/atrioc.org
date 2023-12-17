7d087b084967814478f7dc29a4e350992d58836c
John Rogers <jrhawktwo@gmail.com>
2023-12-17 15:45:30 -0600

    added xword svg

--- src/App.js
+++ src/App.js
@@ -26,9 +26,13 @@ const Content = ({ toggle }) => {
                         <Button svgText="IMM" link="https://www.youtube.com/channel/UCxIkgQxxh_hw-ZVJyAUuNlw" />
 
                         <a
-                            className={`md:hidden flex px-10 py-4 justify-center items-center gap-4 bg-[#2B2B2B] text-white hover:bg-[#353535] font-black md:text-2xl sm:text-base rounded-md`}
+                            className={`md:hidden inline-flex px-10 py-4 justify-center items-center gap-4 bg-[#2B2B2B] text-white hover:bg-[#353535] font-black md:text-2xl sm:text-base rounded-md`}
                             href={"https://aclucrossword.org/"}
                         >
+
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="#FFFFFF" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                            </svg>
                             ACLU CROSSWORD
                         </a>
                     </div>
@@ -42,7 +46,7 @@ const NavLink = () => {
     return (
         <a
             href="https://aclucrossword.org/"
-            className={`flex px-10 py-4 justify-center items-center gap-4 text-white font-black md:text-2xl sm:text-base rounded-md`}
+            className={`inline-flex px-10 py-4 justify-center items-center gap-4 text-white font-black md:text-2xl sm:text-base rounded-md `}
         >
             ACLU CROSSWORD
         </a>
